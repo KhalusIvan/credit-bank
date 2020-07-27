f7c476368244127b8f12626fc95abeceff433717
Khalus Ivan <vakhalus@gmail.com>
2020-07-27 22:12:32 +0300

    make it better

--- dataSet/setUserCredit.js
+++ dataSet/setUserCredit.js
@@ -13,28 +13,35 @@ function setUserCredit(){
     app.post('/setUserCredit', middleware, type, async (req, res) => {
         let startDate = new Date();
         let finishDate = new Date();
-        finishDate.setDate(finishDate.getDate() + req.body.time);
-        let hashString = req.body.email + startDate.toString();
-        bcrypt.hash(hashString, 10, async function(err, hash) {
-            base.collection('active_credits').insertOne({
-                "id": hash,
-                "user": req.user.email,
-                "name": req.body.name,
-                "start_date": startDate,
-                "time": req.body.time,
-                "finish_date": finishDate,
-                "percent": req.body.percent,
-                "start_sum": req.body.start_sum,
-                "already_paid": 0,
-                "finish_sum": req.body.start_sum + (req.body.start_sum / 100 * req.body.percent),
-            },(err,result)=>{
-                if(err)
-                    return console.log(err);
-                res.send(result.ops[0]);
-            }); 
-            //res.send(commentsItem);
-
+        finishDate.setDate(finishDate.getDate() + req.body.term);
+        let hashString = req.user.email + startDate.toString();
+        base.collection('credit_types').find({id:req.body.id).toArray((err,resp)=>{
+            if (err) console.log("eeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrroooooooooooooooooorrrrrrrrrrrrrrrr")
+            bcrypt.hash(hashString, 10, async function(err, hash) {
+                base.collection('users_credits').insertOne({
+                    "id": hash,
+                    "user": req.user.email,
+                    "name": resp.name,
+                    "description": resp.description,
+                    "start_date": startDate,
+                    "term": req.body.term,
+                    "finish_date": finishDate,
+                    "percent": req.body.percent,
+                    "value": req.body.value,
+                    "paid": 0,
+                    "finish_sum": req.body.start_sum + (req.body.start_sum / 100 * req.body.percent * req.body.term),
+                    "fine": 0,
+                    "status": "active"
+                },(err,result)=>{
+                    if(err)
+                        return console.log(err);
+                    res.send(result.ops[0]);
+                }); 
+                //res.send(commentsItem);
+    
+            });
         });
+       
     });
 }
 module.exports.setUserCredit = setUserCredit;
