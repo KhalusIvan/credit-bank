211f2ba79ae64ae85116ae6d8b5fb2043d2d7e11
Khalus Ivan <vakhalus@gmail.com>
2020-07-27 21:50:10 +0300

    make it better

--- dataGet/getUsersCredits.js
+++ dataGet/getUsersCredits.js
@@ -12,6 +12,9 @@ function getUsersCredits(){
     app.post('/getUsersCredits', type, middleware, (req, res) => {
         base.collection('users_credits').find({user: req.user.email}).toArray((err,resp)=>{
             if (err) console.log("eeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrroooooooooooooooooorrrrrrrrrrrrrrrr")
+            resp.map(el => {
+                el.start_date = new Date(el.start_date);
+            })
             res.send(resp);
         });
     });
