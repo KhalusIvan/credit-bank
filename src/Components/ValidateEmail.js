a6d94027b9649a37b49bd1771746771f847885c9
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2020-07-28 16:37:07 +0300

    kgdfooаааi

--- src/Components/ValidateEmail.js
+++ src/Components/ValidateEmail.js
@@ -1,32 +1,50 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Proxy from '../Contexts/Proxy';
 import {
-    Redirect,useParams,useHistory
-  } from "react-router-dom";
+  Redirect, useParams, useHistory
+} from "react-router-dom";
 export default function ValidateEmail(props) {
-    const { token } = useParams();
-    const {proxy} = useContext(Proxy);
-    const history = useHistory()
-    async function fetchData() {
-      let resp = await fetch(proxy + '/checkUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token
-        }
-      });
-      const json = await resp.json();
-      if (json.role === 'user') {
-          localStorage.setItem('token',token);
-          history.push('/user');
-          window.location.reload();
+  const { token } = useParams();
+  const { proxy } = useContext(Proxy);
+  const history = useHistory();
+  const [isValidUser, setIsValidUser] = useState(false);
+  const [counter, setCounter] = useState(5);
+  async function fetchData() {
+    let resp = await fetch(proxy + '/checkUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
       }
-      else{
-        history.push('/');
-      }
+    });
+    const json = await resp.json();
+    if (json.role === 'user') {
+      localStorage.setItem('token', token);
+      setIsValidUser(true);
     }
-    useEffect(()=>{
-        fetchData();
-    },[])
-    return (<h1>Loading...</h1>)
+    else{
+      history.push('/');
+    } 
   }
+  useEffect(()=>{
+    if(setIsValidUser){
+      setTimeout(() => {
+        setCounter(counter - 1);
+      }, 1000);
+    }
+    if(counter === 0){
+      history.push('/user');
+      window.location.reload();
+    }
+  },[isValidUser,counter])
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (<div className='tokenPage'>
+    <div class="spiner-token-page m-4 spinner-border text-light" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <h1 className={`token-title ${isValidUser ? '' : 'd-none'}`}>Ваш аккаунт вже підтверджено</h1>
+    <h4 className={`token-text ${isValidUser ? '' : 'd-none'}`}>Переадресація на особистий кабінет через {counter}</h4>
+  </div>)
+}
