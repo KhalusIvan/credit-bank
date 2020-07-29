256b53662ad8f133f4783fdc2b64dd3c7a75c1c6
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2020-07-29 19:54:03 +0300

    kgdfooаааi

--- src/Components/ValidateEmail.js
+++ src/Components/ValidateEmail.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Proxy from '../Contexts/Proxy';
 import {
-  Redirect, useParams, useHistory
+  useParams, useHistory
 } from "react-router-dom";
+import UserMainPage from './User/UserMainPage';
+import User from '../Contexts/User';
 export default function ValidateEmail(props) {
   const { token } = useParams();
   const { proxy } = useContext(Proxy);
   const history = useHistory();
+  const {changeUserRole} = useContext(User);
   const [isValidUser, setIsValidUser] = useState(false);
   const [counter, setCounter] = useState(5);
   async function fetchData() {
@@ -20,31 +23,38 @@ export default function ValidateEmail(props) {
     const json = await resp.json();
     if (json.role === 'user') {
       localStorage.setItem('token', token);
+      changeUserRole('user');
       setIsValidUser(true);
     }
-    else{
+    else {
       history.push('/');
-    } 
+    }
   }
-  useEffect(()=>{
-    if(setIsValidUser){
+  useEffect(() => {
+    if (setIsValidUser) {
       setTimeout(() => {
-        setCounter(counter - 1);
+          if(counter === 0){
+            return;
+          }
+          else if(counter < 3)
+            window.history.pushState(null,'','/user');
+          setCounter(counter - 1);
       }, 1000);
     }
-    if(counter === 0){
-      history.push('/user');
-      window.location.reload();
-    }
-  },[isValidUser,counter])
+  }, [isValidUser, counter])
   useEffect(() => {
     fetchData();
   }, []);
-  return (<div className='tokenPage'>
-    <div class="spiner-token-page m-4 spinner-border text-light" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
-    <h1 className={`token-title text-center ${isValidUser ? '' : 'd-none'}`}>Ваш аккаунт вже підтверджено</h1>
-    <h4 className={`token-text text-center ${isValidUser ? '' : 'd-none'}`}>Переадресація на особистий кабінет через {counter}</h4>
-  </div>)
+  return (
+    <>
+      <div className={`tokenPage ${counter === 0 ? 'd-none' : ''}`}>
+        <div className="spiner-token-page m-4 spinner-border text-light" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <h1 className={`token-title text-center ${isValidUser ? '' : 'd-none'}`}>Ваш аккаунт вже підтверджено</h1>
+        <h4 className={`token-text text-center ${isValidUser ? '' : 'd-none'}`}>Переадресація на особистий кабінет через {counter}</h4>
+      </div>
+      <UserMainPage/>
+    </>
+  )
 }
