3863c1276f52eccc55550b433e905145593c6533
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2020-08-06 18:59:48 +0300

    kgdfooаааi

--- src/Components/User/UserReviewManagement/UserGiveReviewImputField.js
+++ src/Components/User/UserReviewManagement/UserGiveReviewImputField.js
@@ -30,7 +30,7 @@ export default (props) => {
         if(user.avatar)
             formData.append("file", new Blob([new Uint8Array(user.avatar.data)]), "image.png");
         else
-            formData.append("file", null, "image.png");
+            formData.append("file", new Blob([]), "image.png");
         formData.append('text',imputValue);
         console.log(user.avatar);
         let res = await fetch(proxy+'/setComment', {
