b42e01f781e6d2359d3c2b79109668c990510a17
Khalus Ivan <vakhalus@gmail.com>
2020-08-18 13:16:48 +0300

    make it better

--- auth/sign_in.js
+++ auth/sign_in.js
@@ -4,6 +4,7 @@ let {type} = require('../server.js');
 var base;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { BSONType } = require('mongodb');
 setTimeout(function run() {
     if(base) return;
     var {db} = require('../server.js');
@@ -13,16 +14,25 @@ setTimeout(function run() {
 function signIn(req, res){
     try {
         base.collection('users').find({email: req.body.email}, {projection:{passport:0}}).toArray((err,resp)=>{
-            if (resp.length == 0)
-                res.json({status: "error"});
-            else {
+            if (resp.length == 0) {
+                base.collection("admin").find({email.req.body.email}).toArray((err, respA) => {
+                    if (respA.length == 0) {
+                        res.json({status: "error"});
+                    } else {
+                        const isValid = bcrypt.compareSync(req.body.password, resp[0].password);
+                        if (isValid) {
+                            const token = jwt.sign({email:resp[0].email, role:resp[0].role}, secretJWT, {expiresIn: "1h"});
+                            res.json({token, role:resp[0].role});
+                        }
+                        else {
+                            res.json({status:"error"});
+                        }
+                    }
+                })
+            } else {
                 const isValid = bcrypt.compareSync(req.body.password, resp[0].password);
                 if (isValid) {
-                    if (resp[0].role == "admin") {
-                        const token = jwt.sign({email:resp[0].email, role:resp[0].role}, secretJWT, {expiresIn: "1h"});
-                        res.json({token, role:resp[0].role});
-                    }
-                    else if (resp[0].is_confirmed) {
+                    if (resp[0].is_confirmed) {
                         const token = jwt.sign({email:resp[0].email, role:resp[0].role}, secretJWT, {expiresIn: "1d"});
                         res.json({token, role:resp[0].role});
                     }
